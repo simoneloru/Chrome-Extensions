29566d29186204177eaadf7ef23b0f01fef334ba
Simone Loru <simoneloru@gmail.com>
2014-07-18 17:27:45 +0200

    Update inject.js
    remove console.log

--- facebook_navigator_fixer/src/inject/inject.js
+++ facebook_navigator_fixer/src/inject/inject.js
@@ -2,11 +2,7 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-		console.log("ciao");
-			document.querySelectorAll(".tinyViewport .slim #blueBar.fixed_elem")[0].style.position = "fixed";
-			document.querySelectorAll(".tinyViewport .slim #blueBar.fixed_elem")[0].style.position = "fixed !important";
-			console.log("ciao2");
-			addNewStyle('.tinyViewport .slim #blueBar.fixed_elem {position:fixed !important;}')
+		addNewStyle('.tinyViewport .slim #blueBar.fixed_elem {position:fixed !important;}')
 
 	}
 	}, 10);
